e7a32af81b8044ed57e3578144bf445c733ec41d
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-01-14 14:30:59 +0530

    twosum

---
+++ src/easy/arrays/TwoSum.java
@@ -0,0 +1,28 @@
+package easy.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+    HashMap<Integer,Integer> hm = new HashMap<>();
+
+    for(int i=0;i<nums.length;i++){
+        int req_no = target - nums[i];
+        if(hm.containsKey(req_no)){
+            int [] arr={hm.get(req_no),i};
+            return arr;
+        }
+        hm.put(nums[i],i);
+    }
+    return null;
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        //System.out.println(twoSum(nums,9));
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+
+    }
+    
+}
