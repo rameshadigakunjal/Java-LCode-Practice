b13a3b13fa2b3de46b2d953911118ad881e9746d
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-01-24 21:28:51 +0530

    Leetcode66

--- src/easy/arrays/PlusOne.java
+++ src/easy/arrays/PlusOne.java
@@ -1,5 +1,29 @@
 package easy.arrays;
 
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        // If all digits were 9
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
+
 public class PlusOne {
-    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] digits = {9, 9, 9};   
+        int[] result = solution.plusOne(digits);
+        for (int digit : result) {
+            System.out.print(digit + " ");
+        }
+    }
 }
