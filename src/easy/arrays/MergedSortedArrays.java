cb442a96c2664b864877ecd27f3bff2ed8d779f9
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-01-26 21:47:06 +0530

    leet88

---
+++ src/easy/arrays/MergedSortedArrays.java
@@ -0,0 +1,47 @@
+package easy.arrays;
+
+import java.util.Arrays;
+
+public class MergedSortedArrays{
+    public static void main(String[] args) {
+Solution s = new Solution();
+
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        s.merge(nums1, m, nums2, n);
+
+        // Print merged array
+        System.out.println(Arrays.toString(nums1));
+            
+    }
+}
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1;       // pointer for nums1
+        int j = n - 1;       // pointer for nums2
+        int k = m + n - 1;   // pointer for merged array
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // Copy remaining elements of nums2 (if any)
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
