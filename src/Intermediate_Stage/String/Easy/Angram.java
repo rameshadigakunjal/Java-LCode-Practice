07e4dd36c457f977b03b8c17ab6c701cb205d4ce
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-02-06 08:33:10 +0530

    leet242

---
+++ src/Intermediate_Stage/String/Easy/Angram.java
@@ -0,0 +1,34 @@
+package Intermediate_Stage.String.Easy;
+
+public class Angram {
+    public boolean isAnagram(String s, String t) {
+        int m=s.length();
+        int n=t.length();
+        if(m!=n){
+         //   System.out.println("Not Anagram");
+         return false;
+        }
+        else{
+            int count[]=new int[26];
+            for(int i=0;i<m;i++){
+                count[s.charAt(i)-'a']++;
+            }
+            for(int i=0;i<n;i++){
+                count[t.charAt(i)-'a']--;
+            }
+            for(int i=0;i<26;i++){
+                if(count[i]!=0){
+                   // System.out.println("Not Anagram");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    public static void main(String[] args) {
+      String s = "anagram";
+        String t = "nagaram";
+        Angram an = new Angram();
+        System.out.println(an.isAnagram(s, t)); // Output: true
+    }
+}
