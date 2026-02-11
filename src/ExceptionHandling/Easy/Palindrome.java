02226a46c06eab22bfa6032c88d5593cef8e5d47
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-02-11 21:24:13 +0530

    day9

---
+++ src/ExceptionHandling/Easy/Palindrome.java
@@ -0,0 +1,32 @@
+package ExceptionHandling.Easy;
+
+public class Palindrome {
+    public boolean isPalindrome(int x) {
+        
+        // Negative numbers are not palindrome
+        // Numbers ending with 0 (except 0 itself) are not palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        
+        int reversedHalf = 0;
+        
+        // Reverse half of the number
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x = x / 10;
+        }
+        
+        // For even digits: x == reversedHalf
+        // For odd digits: x == reversedHalf / 10 (middle digit ignored)
+        return (x == reversedHalf || x == reversedHalf / 10);
+}
+
+
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+        int input = 121;
+        boolean result = palindrome.isPalindrome(input);    
+        System.out.println("Is " + input + " a palindrome? " + result);
+    }
+}
