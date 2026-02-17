8875d51ef6ccfdc5c8ef63614ac594c46660b6c1
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-02-17 14:45:44 +0530

    leet349

--- src/Collection/Easy/IntersectionofTwoArrays.java
+++ src/Collection/Easy/IntersectionofTwoArrays.java
@@ -1,6 +1,41 @@
 package Collection.Easy;
 
+import java.util.HashSet;
+
 public class IntersectionofTwoArrays {
-    
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> resultSet = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+
+        for (int num : resultSet) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        IntersectionofTwoArrays solution = new IntersectionofTwoArrays();
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        int[] result = solution.intersection(nums1, nums2);
+        System.out.print("Intersection: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
 }
 
