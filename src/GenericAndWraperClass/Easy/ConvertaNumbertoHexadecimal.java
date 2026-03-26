505e7b506af353a1636825745d5d684fca910f14
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-03-26 22:08:06 +0530

    leet307

---
+++ src/GenericAndWraperClass/Easy/ConvertaNumbertoHexadecimal.java
@@ -0,0 +1,26 @@
+package GenericAndWraperClass.Easy;
+
+import java.util.Scanner;
+
+class convertNumberToHexadecimal {
+    public String toHex(int num) {
+        if (num == 0) return "0";
+
+        char[] hex = "0123456789abcdef".toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        while (num != 0) {
+            result.append(hex[num & 15]);  // last 4 bits
+            num >>>= 4;  // unsigned right shift
+        }
+
+        return result.reverse().toString();
+    }
+    public static void main(String[] args) {
+        convertNumberToHexadecimal solution = new convertNumberToHexadecimal();
+        Scanner sc=new Scanner(System.in);
+        int num = sc.nextInt();
+        String result = solution.toHex(num);
+        System.out.println(result); // Output: "1a"
+    }
+}
